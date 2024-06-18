3ea3e026d3b79bd21785116d01f21bdbf80380ef
Vijaya Malla <mallabiju@gmail.com>
2024-06-18 09:29:58 +0545

    Socket Programming client and server

--- Z_Socket_Programming/a_client_server/Server.java
+++ Z_Socket_Programming/a_client_server/Server.java
@@ -11,15 +11,40 @@ public class Server {
     private DataInputStream in = null;
 
     // constructor with port
-    public Server(int port)
-    {
+    public Server(int port) {
         // starts the server and waits for connection
         try {
             server = new ServerSocket(port);
             System.out.println("Server Started");
             System.out.println("Waiting for client.......");
-        } catch (Exception e) {
-            // TODO: handle exception
+            socket = server.accept();
+            System.out.println("Client accepted");
+
+            // takes input from the client socket
+            in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            String line = "";
+
+            // reads message from client until "Over" is sent
+            while (!line.equals("Over")) {
+                try {
+                    line = in.readUTF();
+                    System.out.println(line);
+
+                } catch (IOException i) {
+                    System.out.println(i);
+                }
+            }
+            System.out.println("Closing connection....");
+            // close connection
+            socket.close();
+            in.close();
+        } catch (IOException i) {
+            System.out.println(i);
         }
+    }
+
+    public static void main(String[] args) {
+        // Server server = new Server(5000);
+        new Server(5000);
     }
 }
